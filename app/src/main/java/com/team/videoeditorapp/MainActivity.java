d674e73f0804fe1b0a50e2c9a0ddb2fab11bb168
Sahil Singh <sahilsingh08062001@gmail.com>
2021-07-03 20:29:56 +0530

    login sdks added
    Co-Authored-By: Sahil Singh <sahilsingh08062001@gmail.com>

--- app/src/main/java/com/team/videoeditorapp/MainActivity.java
+++ app/src/main/java/com/team/videoeditorapp/MainActivity.java
@@ -3,12 +3,16 @@ package com.team.videoeditorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button fb,gmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fb=(Button)findViewById(R.id.fb_login);
+        gmail=(Button)findViewById(R.id.gmail_login);
+
     }
 }
